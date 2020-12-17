15770ecd6b26b9a2c0146cc649c88463cf77e487
JulioCesarAM <fhoenixeterno@gmail.com>
2020-12-18 00:09:29 +0100

    Cambiosº
    programa funcionando

--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -2,104 +2,97 @@ package modelo;
 
 public class Main {
 
-	private int filas = 50, columnas = 50;
-	private boolean matriz[][] = new boolean[filas][columnas];
+	private static boolean matriz[][];
 
 	public static void main(String[] args) {
 
+		matriz = new boolean[5][5];
+
+		matriz[2][3] = true;
+		matriz[2][2] = true;
+		matriz[2][1] = true;
+
+		for (int i = 0; i < 5; i++) {
+			imprimirMatriz(matriz, i);
+			matriz = siguienteIteracion();
+
+		}
 	}
 
-	public boolean[][] siguienteIteracion(boolean matriz[][]) {
-
-		boolean auxiliar[][] = new boolean[3][3];
-		boolean matrizFutura[][] = new boolean[50][50]; // refrescamos la matriz anterior con esta nueva
-
+	public static void imprimirMatriz(boolean matriz[][], int iter) {
+		String cell = "";
 		for (int fMatriz = 0; fMatriz < matriz.length; fMatriz++) {
 
 			for (int cMatriz = 0; cMatriz < matriz[fMatriz].length; cMatriz++) {
+				cell = matriz[fMatriz][cMatriz] ? "1" : "0";
+				System.out.print(cell);
+			}
+			System.out.println();
+		}
+		System.out.println(iter + ". ---------------------");
+	}
+
+	public static boolean[][] siguienteIteracion() {
+
+		boolean auxiliar[][] = new boolean[3][3];
+		boolean matrizFutura[][] = new boolean[matriz.length][matriz[0].length]; // refrescamos la matriz anterior con
+																					// esta nueva
+
+		for (int fMatriz = 1; fMatriz < matriz.length - 1; fMatriz++) {
+
+			for (int cMatriz = 1; cMatriz < matriz[fMatriz].length - 1; cMatriz++) {
 
 				for (int fAuxiliar = 0; fAuxiliar < auxiliar.length; fAuxiliar++) {
 
 					for (int cAuxiliar = 0; cAuxiliar < auxiliar[fAuxiliar].length; cAuxiliar++) {
 
-						auxiliar[fAuxiliar][cAuxiliar] = matriz[fMatriz + fAuxiliar - 1][cMatriz + cAuxiliar - 1]; // fMatriz
-																													// =
-																													// 3
-																													// /
-																													// cMatriz
-																													// =
-																													// 3
+						auxiliar[fAuxiliar][cAuxiliar] = matriz[fMatriz + fAuxiliar - 1][cMatriz + cAuxiliar - 1];
 
 					}
 
 				}
-
 				matrizFutura[fMatriz][cMatriz] = comprobarCelulasVecinas(auxiliar);
 
 			}
 
 		}
 
-		return matriz;
+		return matrizFutura;
 	}
 
-	public boolean comprobarCelulasVecinas(boolean matrizVecina[][]) {
+	public static boolean comprobarCelulasVecinas(boolean matrizVecina[][]) {
 
 		/*
-		 * Una cÃ©lula muerta con exactamente 3 cÃ©lulas vecinas vivas "nace" (es decir,
-		 * al turno siguiente estarÃ¡ viva).
+		 * Una célula muerta con exactamente 3 células vecinas vivas "nace" (es decir,
+		 * al turno siguiente estará viva).
 		 * 
-		 * Una cÃ©lula viva con 2 o 3 cÃ©lulas vecinas vivas sigue viva, en otro caso
-		 * muere (por "soledad" o "superpoblaciÃ³n").
+		 * Una célula viva con 2 o 3 células vecinas vivas sigue viva, en otro caso
+		 * muere (por "soledad" o "superpoblación").
 		 */
 
 		int cont = 0;
+		int i = 0;
 
-		for (int i = 0; i < matrizVecina.length; i++) {
+		for (i = 0; i < matrizVecina.length; i++) {
 
 			for (int j = 0; j < matrizVecina[i].length; j++) {
 
-				if (matrizVecina[i][j] == true & i != 1 & j != 1) {
-
+				if (matrizVecina[i][j] == true & !(i==1 & j==1)) {
 					cont++;
-
 				}
 
 			}
 
 		}
 
-		if ((cont == 2 | cont == 3) && matrizVecina[1][1] == true) // si estÃ¡ viva (matrizVecina == yoMismo)
-
+		boolean celula=matrizVecina[1][1];
+		if (cont == 3 && !celula)
 			return true;
 
-		if (cont == 3) // si estÃ¡ muerta
-
+		if ((cont == 2 || cont == 3) && celula)
 			return true;
 
 		return false;
 
 	}
-
-	public int contarCelulasVecinas() {
-
-		boolean auxiliar[][] = new boolean[3][3];
-
-		for (int fAuxiliar = 0; fAuxiliar < auxiliar.length; fAuxiliar++) {
-
-			for (int cAuxiliar = 0; cAuxiliar < auxiliar[fAuxiliar].length; cAuxiliar++) {
-
-				auxiliar[fAuxiliar][cAuxiliar] = matriz[fMatriz + fAuxiliar - 1][cMatriz + cAuxiliar - 1]; // fMatriz =
-																											// 3 /
-																											// cMatriz =
-																											// 3
-
-			}
-
-		}
-
-		return 0;
-
-	}
-
 }
